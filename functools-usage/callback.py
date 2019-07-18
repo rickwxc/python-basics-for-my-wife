d2ca9b18f8f3c44d56642449a29fa055759b1792
Rick Wang <lurker2000wxc@gmail.com>
2019-07-18 17:38:12 +1000

    cb

--- functools-usage/callback.py
+++ functools-usage/callback.py
@@ -1,10 +1,15 @@
 from functools import partial
 
 def prt(cb):
-    print("done")
-    cb()
+    print("done: ", cb())
+
 
 def cb(x, y):
-    print(100 + x + y)
+    return(100 + x + y)
+
+def cb2(data):
+    return(100 + data)
 
 prt(partial(cb, x=1, y=2))
+
+prt(partial(cb2, data=11))
